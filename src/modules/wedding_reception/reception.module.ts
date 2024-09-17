073250220b4b14e354277811103c6e15a2ec94e6
fadilahagiel@gmail.com <fadilahagiel@gmail.com>
2024-09-17 09:44:26 +0700

    update router

--- src/modules/wedding_reception/reception.module.ts
+++ src/modules/wedding_reception/reception.module.ts
@@ -9,6 +9,10 @@ router.get("/all", UserJwtVerify.adminVerify, ReceptionController.GetAllReceptio
 router.get("/:id", UserJwtVerify.adminVerify, ReceptionController.GetOneReception)
 router.delete("/:id", UserJwtVerify.adminVerify, ReceptionController.DeleteOneReception)
 router.patch("/:id", UserJwtVerify.adminVerify, ReceptionController.UpdateOneReception)
+router.patch("/status/cancel/:id", UserJwtVerify.adminVerify, ReceptionController.UpdateCancelReception)
+router.patch("/status/complete/:id", UserJwtVerify.adminVerify, ReceptionController.UpdateCompleteReception)
+router.patch("/status/in_progress/:id", UserJwtVerify.adminVerify, ReceptionController.UpdateInProgressReception)
+
 router.get("/user/:id", UserJwtVerify.adminVerify, ReceptionController.GetReceptionByUser)
 router.post('/create', UserJwtVerify.adminVerify, CreateReceptionDto, ReceptionController.CreateReception)
 
